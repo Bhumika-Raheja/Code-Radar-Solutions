87871e45c9f58f1fba74cfcecc02edf037b8eff0
Bhumika-Raheja <bhumikaraheja2810@gmail.com>
2025-01-30 17:23:37 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char gr;
+    scanf("%c",&gr);
+    if (gr=='A'){
+        printf("Excellent");
+    }
+    else {
+        printf("Invalid grade")
+    }
+    return 0;
+}
