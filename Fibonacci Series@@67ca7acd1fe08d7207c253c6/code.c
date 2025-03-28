d1fccca26eb53710f23a5c2a5e4732bf9dc61323
Bhumika Raheja <bhumikaraheja2810@gmail.com>
2025-03-28 18:21:00 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,6 @@
-void fibonacciSeries(int a){
-    int a=0,b=1;
-    for (int i=0;i<a;i++){
+void fibonacciSeries(int n){
+    int a=0,b=1,c;
+    for (int i=0;i<n;i++){
         c=a+b;
         printf("%d ",c);
         a=b;
